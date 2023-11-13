07271f366642b6cb5bace8c173e0b6ee759b08de
apurvpatel2121 <apurvpatel2121@gmail.com>
2023-11-13 14:07:31 +0000

    optimized some functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,49 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
-import { accessories } from "./assets/data"
+import data from "./assets/data"
 import { toPng } from 'html-to-image'
 import download from 'downloadjs'
 
 function App() {
-  const [data, setData] = useState(accessories)
   const [index, setIndex] = useState(0)
-  const [bg, setBg] = useState("default")
-  const [ears, setEars] = useState("default")
-  const [hair, setHair] = useState("default")
-  const [neck, setNeck] = useState("default")
-  const [nose, setNose] = useState("default")
-  const [mouth, setMouth] = useState("default")
-  const [eyes, setEyes] = useState("default")
-  const [leg, setLeg] = useState("default")
-  const [accessorie, setAccessorie] = useState("default")
-  const [accessorie2, setAccessorie2] = useState("default")
+  const [images, setImages] = useState({})
 
   useEffect(() => {
-    const dataClone = [...data]
-    dataClone.map(attr => {
-      attr.items[0].selected = true
+    // assign default to each object
+    const defaultImages = {}
+    data.forEach((item) => {
+      defaultImages[item.label] = { img: "default", btn: 0 }
     })
-    setData(dataClone)
+    setImages(defaultImages)
   }, [])
 
-
-  function downloadImage(){
-    console.log("clicked")
+  function downloadImage() {
     const alpacaCanvasNode = document.getElementById('mainSection')
     toPng(alpacaCanvasNode).then(dataUrl => {
       download(dataUrl, 'my-alpaca.png')
     })
   }
 
-
-  function handleChange(item,i) {
-
-    data[index].items.map(i => i.selected = false)
-    item.selected = true
-
-    switch (data[index].label) {
-      case "backgrounds":
-        setBg(item.img)
-        break;
-      case "ears":
-        setEars(item.img)
-        break;
-      case "hair":
-        setHair(item.img)
-        break;
-      case "neck":
-        setNeck(item.img)
-        break;
-      case "nose":
-        setNose(item.img)
-        break;
-      case "mouth":
-        setMouth(item.img)
-        break;
-      case "eyes":
-        setEyes(item.img)
-        break;
-      case "leg":
-        setLeg(item.img)
-        break;
-      case "accessories":
-        setAccessorie(item.img)
-        break;
-      case "accessories2":
-        setAccessorie2(item.img)
-        break;
-
-      default:
-        break;
-    }
+  function handleChange(item, i) {
+    setImages((prevImages) => ({
+      ...prevImages,
+      [data[index].label]: {
+        ...prevImages[data[index].label],
+        img: item,
+        btn: i,
+      },
+    }));
   }
+
+
+  function getImageUrl(item) {
+    const imageUrl = useMemo(() => {
+      return `images/${item.label}/${images[item.label]?.img ?? 'default'}.png`
+    }, [images])
+    return imageUrl;
+  }
+
+
 
   return (
     <div className='main'>
@@ -86,67 +53,66 @@ function App() {
         <h1 className='header'>Alpaca Image Generator</h1>
       </nav>
 
-    <div className='container'>
-      
-      <div className='leftContainer'>
-        <section className='mainSection' id='mainSection'>
-          <img src={"images/backgrounds/" + bg + ".png"} alt="" className='background' />
-          <img src={"images/ears/" + ears + ".png"} alt="" className='ears' />
-          <img src={"images/hair/" + hair + ".png"} alt="" className='hair' />
-          <img src={"images/neck/" + neck + ".png"} alt="" className='neck' />
-          <img src={"images/" + nose + ".png"} alt="" className='nose' />
-          <img src={"images/mouth/" + mouth + ".png"} alt="" className='mouth' />
-          <img src={"images/eyes/" + eyes + ".png"} alt="" className='eyes' />
-          <img src={"images/leg/" + leg + ".png"} alt="" className='leg' />
-          <img src={"images/accessories/" + accessorie + ".png"} alt="" className='accessories' />
-          <img src={"images/accessories/" + accessorie2 + ".png"} alt="" className='accessories' />
-        </section>
+      <div className='container'>
 
-        <div className='download'>
-          <button className='btn downloadBtn' onClick={()=>downloadImage()}>Download</button>
+        <div className='leftContainer'>
+          <section className='mainSection' id='mainSection'>
+            {data.map((item) => {
+              return (
+                <img
+                  key={item.label}
+                  // src={`images/${item.label}/${images[item.label].img}.png`}
+                  src={getImageUrl(item)}
+                  className={`${item.label}`}
+                />
+              )
+            })}
+          </section>
+
+          <div className='download'>
+            <button className='btn downloadBtn' onClick={() => downloadImage()}>Download</button>
+          </div>
         </div>
-      </div>
 
-      <div className='rightContainer'>
-        <section className='buttonSection'>
-          <div className='accessorizeContainer'>
-            <h1 className='title'>Accessorize your Alpaca</h1>
-            <div className='buttons'>
-              {
-                data.map((attribute, i) => {
-                  return (
-                    <button key={i} className={index===i ? "btn active" : "btn"}
-                      onClick={() => {
-                        setIndex(i)
-                      }}>{attribute.label}</button>
-                  )
-                })
-              }
+        <div className='rightContainer'>
+          <section className='buttonSection'>
+            <div className='accessorizeContainer'>
+              <h1 className='title'>Accessorize your Alpaca</h1>
+              <div className='buttons'>
+                {
+                  data.map((attribute, i) => {
+                    return (
+
+                      <button key={i} className={index === i ? "btn active" : "btn"}
+                        onClick={() => {
+                          setIndex(i)
+                        }}>{attribute.label}</button>
+                    )
+                  })
+                }
+              </div>
             </div>
-          </div>
-          <div>
-            <hr />
-          </div>
-          <div className='backgroundContainer'>
-            <h1 className='title'>{data[index].label}</h1>
-            <div className='buttons'>
-              {
-                data[index].items.map((item, i) => {
-                  return (
-                    <button key={i} className={item.selected ? "btn active" : "btn"} onClick={(e) => {
-                      handleChange(item,i)
-                    }}>{item.img}</button>
-                  )
-                })
-              }
+            <div>
+              <hr />
             </div>
-          </div>
-        </section>
+            <div className='backgroundContainer'>
+              <h1 className='title'>{data[index].label}</h1>
+              <div className='buttons'>
+                {
+                  data[index].items.map((item, i) => {
+                    return (
+                      <button key={item} className={images[data[index].label]?.btn === i ? "btn active" : "btn"} onClick={(e) => {
+                        handleChange(item, i)
+                      }}>{item}</button>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </section>
+        </div>
+
       </div>
-
-
-      </div>
-
     </div>
   )
 }
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,130 +1,40 @@
-export const accessories = [
-    {
-      id: 0,
-      label: "backgrounds",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "blue50", selected: false },
-        { id: 2, img: "blue60", selected: false },
-        { id: 3, img: "blue70", selected: false },
-        { id: 4, img: "green60", selected: false },
-        { id: 5, img: "green70", selected: false },
-        { id: 6, img: "grey40", selected: false },
-        { id: 7, img: "grey70", selected: false },
-        { id: 8, img: "grey80", selected: false },
-        { id: 9, img: "red50", selected: false },
-        { id: 10, img: "red60", selected: false },
-        { id: 11, img: "red70", selected: false },
-        { id: 13, img: "yellow50", selected: false },
-        { id: 14, img: "yellow60", selected: false },
-        { id: 15, img: "yellow70", selected: false },
-      ]
-    },
-    {
-      id: 1,
-      label: "ears",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "tilt-backward", selected: false },
-        { id: 2, img: "tilt-forward", selected: false }
-      ]
-    },
-    {
-      id: 2,
-      label: "hair",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "bang", selected: false },
-        { id: 2, img: "curls", selected: false },
-        { id: 3, img: "elegant", selected: false },
-        { id: 4, img: "fancy", selected: false },
-        { id: 5, img: "short", selected: false },
-      ]
-    },
-    {
-      id: 3,
-      label: "neck",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "bend-backward", selected: false },
-        { id: 2, img: "bend-forward", selected: false },
-        { id: 3, img: "thick", selected: false },
-      ]
-    },
-    {
-      id: 4,
-      label: "nose",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false }
-      ]
-    },
-    {
-      id: 5,
-      label: "mouth",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "astonished", selected: false },
-        { id: 2, img: "eating", selected: false },
-        { id: 3, img: "laugh", selected: false },
-        { id: 4, img: "tongue", selected: false },
-      ]
-    },
-    {
-      id: 6,
-      label: "eyes",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "angry", selected: false },
-        { id: 2, img: "naughty", selected: false },
-        { id: 3, img: "panda", selected: false },
-        { id: 4, img: "smart", selected: false },
-        { id: 5, img: "star", selected: false },
-      ]
-    },
-    {
-      id: 7,
-      label: "leg",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "bubble-tea", selected: false },
-        { id: 2, img: "cookie", selected: false },
-        { id: 3, img: "game-console", selected: false },
-        { id: 4, img: "tilt-backward", selected: false },
-        { id: 5, img: "tilt-forward", selected: false },
-      ]
-    },
-    {
-      id: 8,
-      label: "accessories",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "earings", selected: false },
-        { id: 2, img: "flower", selected: false },
-        { id: 3, img: "glasses", selected: false },
-        { id: 4, img: "headphone", selected: false },
-      ]
-    },
-    {
-      id: 9,
-      label: "accessories2",
-      selected: false,
-      items: [
-        { id: 0, img: "default", selected: false },
-        { id: 1, img: "earings", selected: false },
-        { id: 2, img: "flower", selected: false },
-        { id: 3, img: "glasses", selected: false },
-        { id: 4, img: "headphone", selected: false },
-      ]
-    },
+const accessoriesData = [
+  {
+    label: "backgrounds",
+    items: ["default", "blue50", "blue60", "blue70", "green60", "green70", "grey40", "grey70", "grey80", "red50", "red60", "red70", "yellow50", "yellow60", "yellow70"]
+  },
+  {
+    label: "ears",
+    items: ["default", "tilt-backward", "tilt-forward"]
+  },
+  {
+    label: "hair",
+    items: ["default", "bang", "curls", "elegant", "fancy", "short"]
+  },
+  {
+    label: "neck",
+    items: ["default", "bend-backward", "bend-forward", "thick"]
+  },
+  {
+    label: "nose",
+    items: ["default"]
+  },
+  {
+    label: "mouth",
+    items: ["default", "astonished", "eating", "laugh", "tongue"]
+  },
+  {
+    label: "eyes",
+    items: ["default", "angry", "naughty", "panda", "smart", "star"]
+  },
+  {
+    label: "leg",
+    items: ["default", "bubble-tea", "cookie", "game-console", "tilt-backward", "tilt-forward"]
+  },
+  {
+    label: "accessories",
+    items: ["default", "earings", "flower", "glasses", "headphone"]
+  },
+];
 
-  ];
-
+export default accessoriesData;
